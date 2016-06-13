02f4c0de1a60225b911cd90b70ec73c846f2192f
Kolodziejczyk Jakub <k.jakub@lexues.co.jp>
2016-06-13 22:16:42 +0200

    Stubbed out detection script

---
+++ scripts/detection.py
@@ -0,0 +1,26 @@
+"""
+This is the "main" script of the entire program.
+It reads in a net for recognizing templates and uses them to detect
+templates in real time in camera images.
+"""
+
+import cv2
+
+def main():
+
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+
+        _, frame = video_capture.read()
+
+        cv2.imshow("image", frame)
+
+        key = cv2.waitKey(30)
+
+        # If spacebar was pressed
+        if key == 32:
+            break
+
+if __name__ == "__main__":
+    main()
